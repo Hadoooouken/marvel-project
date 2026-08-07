3989c71f5d69e6150475e08b8c38221655e7d727
Filipp <wolverinexdd@yandex.ru>
2026-08-07 16:24:43 +0400

    добавлен прелоадер и обработка ошибок запросов

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,8 +1,10 @@
 import './randomChar.scss';
 import MarvelService from '../../services/MarvelService';
-
 import mjolnir from '../../resources/img/mjolnir.png';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
 
 class RandomChar extends Component {
   constructor(props) {
@@ -11,49 +13,34 @@ class RandomChar extends Component {
   }
   state = {
     char: {},
+    loading: true,
+    error: false,
   };
 
   marvelService = new MarvelService();
 
   onCharLoaded = (char) => {
-    this.setState({ char });
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
   };
 
   updateChar = () => {
     const id = Math.floor(Math.random() * 20);
-    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+    this.marvelService.getCharacter(id).then(this.onCharLoaded).catch(this.onError);
   };
 
   render() {
-    const {
-      char: { name, description, thumbnail, homePage, wiki },
-    } = this.state;
-
-    const shortDescription =
-      description?.length > 47 ? `${description.slice(0, 47)}...` : description;
-
-    const visibleDescription = shortDescription
-      ? shortDescription
-      : 'для этого персонажа нет описания';
+    const { char, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View char={char} /> : null;
 
     return (
       <div className="randomchar">
-        <div className="randomchar__block">
-          <img src={thumbnail} alt="Random character" className="randomchar__img" />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-
-            <p className="randomchar__descr">{visibleDescription}</p>
-            <div className="randomchar__btns">
-              <a href={homePage} className="button button__main">
-                <div className="inner">homePage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
+        {errorMessage} {spinner} {content}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
@@ -70,5 +57,34 @@ class RandomChar extends Component {
     );
   }
 }
+
+const View = ({ char }) => {
+  const { name, description, thumbnail, homePage, wiki } = char;
+  const shortDescription =
+    description?.length > 47 ? `${description.slice(0, 47)}...` : description;
+
+  const visibleDescription = shortDescription
+    ? shortDescription
+    : 'для этого персонажа нет описания';
+
+  return (
+    <div className="randomchar__block">
+      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+
+        <p className="randomchar__descr">{visibleDescription}</p>
+        <div className="randomchar__btns">
+          <a href={homePage} className="button button__main">
+            <div className="inner">homePage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RandomChar;
---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,22 @@
+import './spinner.scss';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-bean-eater-nq4q5u6dq7r">
+      <div className="ldio-x2uulkbinbj">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
